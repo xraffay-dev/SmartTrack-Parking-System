ab4f449bc749416986085604bc4b11c53a283117
AbdulSamiWaseem <abdulsamiwaseem26@gmail.com>
2025-05-11 16:09:00 +0500

    changes

--- parking/apps.py
+++ parking/apps.py
@@ -4,15 +4,3 @@ from django.apps import AppConfig
 class ParkingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "parking"
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
-        # Generate the URL for the vehicle's detail page based on license_plate
